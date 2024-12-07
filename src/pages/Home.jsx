3dcb3495379cfc6500b941914d5f160eb0faf3c0
MogiliManikanta <you@example.com>
2024-12-07 20:12:24 +0530

    fourth

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,30 @@
+import { Categories } from "../assets/mockData";
+import HeroPng from "../assets/images/Hero.png";
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <div>
+        <div>
+          <div>
+            <div>Shop By Category</div>
+            <ul>
+              {Categories.map((category, index) => (
+                <li key={index}>
+                  <div></div>
+                  {category}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <img src={HeroPng} alt="heropng" />
+            <p> Mogili Mnikanta </p>
+            <h2>WELCOME TO E_SHOP</h2>
+            <p>MILLIONS+ PRODUCTS</p>
+            <button>Shop Now </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
