88b312c6e5d4baa6524d2eda6b020433c4c61206
MogiliManikanta <you@example.com>
2024-12-09 13:39:06 +0530

    done

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 function CheckOut() {
   const [billingToggle, setBillingToggle] = useState(true);
   const [shippingToggle, setShippingToggle] = useState(false);
   const [paymentToggle, setPaymentToggle] = useState(false);
   const [paymentMethod, setPaymentMethod] = useState("cod");
+  const [shippingInfo, setShippingInfo] = useState({
+    address: "",
+    city: "",
+    zip: "",
+  });
+  const cart = useSelector((state) => state.cart);
   return (
     <div className="container mx-auto py-8 min-h-96 px-4 md:px-16 lg:px-24">
       <h3 className="text-2xl font-semibold mb-4">CHECKOUT</h3>
@@ -70,6 +77,12 @@ function CheckOut() {
                     name="address"
                     placeholder="Enter Address"
                     className="w-full px-3 py-2 border rounded"
+                    onChange={(e) =>
+                      setShippingInfo({
+                        ...shippingInfo,
+                        address: e.target.value,
+                      })
+                    }
                   />
                 </div>
                 <div>
@@ -79,62 +92,32 @@ function CheckOut() {
                     name="city"
                     placeholder="Enter City"
                     className="w-full px-3 py-2 border rounded"
+                    onChange={(e) =>
+                      setShippingInfo({
+                        ...shippingInfo,
+                        city: e.target.value,
+                      })
+                    }
                   />
                 </div>
                 <div>
                   <label className="block text-gray-700">Zip Code</label>
                   <input
-                    type="number"
+                    type="text"
                     name="zip"
                     placeholder="Enter Zip Code"
                     className="w-full px-3 py-2 border rounded"
+                    onChange={(e) =>
+                      setShippingInfo({
+                        ...shippingInfo,
+                        zip: e.target.value,
+                      })
+                    }
                   />
                 </div>
               </div>
             )}
           </div>
-
-          {/**Payment Method */}
-          {/*<div className="border p-4 mb-6 rounded">
-            <div
-              className="flex items-center justify-between cursor-pointer"
-              onClick={() => setPaymentToggle(!paymentToggle)}
-            >
-              <h3 className="text-lg font-semibold">Payment Information</h3>
-              {paymentToggle ? <FaAngleDown /> : <FaAngleUp />}
-            </div>
-            {paymentToggle && (
-              <div className="space-y-4 mt-4">
-                <div>
-                  <label className="block text-gray-700">Address</label>
-                  <input
-                    type="text"
-                    name="address"
-                    placeholder="Enter Address"
-                    className="w-full px-3 py-2 border rounded"
-                  />
-                </div>
-                <div>
-                  <label className="block text-gray-700">City</label>
-                  <input
-                    type="text"
-                    name="city"
-                    placeholder="Enter City"
-                    className="w-full px-3 py-2 border rounded"
-                  />
-                </div>
-                <div>
-                  <label className="block text-gray-700">Zip Code</label>
-                  <input
-                    type="number"
-                    name="zip"
-                    placeholder="Enter Zip Code"
-                    className="w-full px-3 py-2 border rounded"
-                  />
-                </div>
-              </div>
-            )}
-          </div>*/}
 
           {/** Payment Option */}
           <div className="border p-4 mb-6 rounded">
@@ -225,7 +208,42 @@ function CheckOut() {
             )}
           </div>
         </div>
-        <div className="md:w-1/3 bg-white p-6 rounded-lg shadow-md border"></div>
+        <div className="md:w-1/3 bg-white p-6 rounded-lg shadow-md border">
+          <h3 className="text-lg font-semibold mb-4">Order Summary</h3>
+          <div className="space-y-4">
+            {cart.products.map((product) => (
+              <div key={product.id} className="flex justify-between">
+                <div className="flex items-center">
+                  <img
+                    src={product.image}
+                    alt=""
+                    className="w-16 h-16 object-contain rounded"
+                  />
+                  <div className="ml-4">
+                    <h4 className="text-md font-semibold">{product.name}</h4>
+                    <p className="text-gray-600">
+                      ₹{product.price} X {product.quantity}
+                    </p>
+                  </div>
+                </div>
+                <div className="text-gray-800">
+                  ₹{product.price * product.quantity}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="mt-4 border-t pt-4">
+            <div className="flex justify-between">
+              <span>Total Price : </span>
+              <span className="font-semibold">
+                ₹{cart.totalPrice.toFixed(2)}
+              </span>
+            </div>
+          </div>
+          <button className="w-full bg-red-600 text-white py-2 mt-6 hover:bg-red-800">
+            Place Order
+          </button>
+        </div>
       </div>
     </div>
   );
